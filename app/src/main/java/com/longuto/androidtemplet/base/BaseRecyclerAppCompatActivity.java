7f296b543890d8aaf184139e922c1d411a3bcb73
longuto <1592404240@qq.com>
2018-10-18 19:53:21 +0800

    mvp - 部分更改

---
+++ app/src/main/java/com/longuto/androidtemplet/base/BaseRecyclerAppCompatActivity.java
@@ -0,0 +1,39 @@
+package com.longuto.androidtemplet.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
+
+import com.longuto.androidtemplet.R;
+
+import butterknife.BindView;
+
+/**
+ * Author by yltang,
+ * Date on 2018/10/13.
+ * PS: RecyclerView含有下拉刷新，上拉加载更多的基类
+ */
+public class BaseRecyclerAppCompatActivity extends BaseAppCompatActivity {
+
+    @BindView(R.id.recycler)
+    RecyclerView recyclerView;
+    @BindView(R.id.refresh)
+    SwipeRefreshLayout mRefresh;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_recycler;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initRecyerView();
+    }
+
+    // 初始化Recy和Refresh
+    private void initRecyerView() {
+
+    }
+}
---
+++ app/src/main/java/com/longuto/androidtemplet/model/local/UserInfo.java
@@ -0,0 +1,28 @@
+package com.longuto.androidtemplet.model.local;
+
+/**
+ * Author by yltang,
+ * Date on 2018/10/13.
+ * PS: 用户信息实体类
+ */
+public class UserInfo {
+
+    private int id; // 用户id
+    private String name;    // 姓名
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
